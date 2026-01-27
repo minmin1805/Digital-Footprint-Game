ebc73faf02b09f685ddca84de4fed49d508091c7
minmin1805 <doanlyminh2005@gmail.com>
2026-01-27 02:10:53 -0500

    setup backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,15 @@
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
 
-const __dirname = path.resolve();
+const PORT = process.env.PORT || 5000;
 
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
+
+export default app;
