b3632b1bad4c62d38dc5496caf472cf4a8c478ff
minmin1805 <doanlyminh2005@gmail.com>
2026-01-27 16:59:56 -0500

    added the json file for 20 posts content

--- frontend/src/data/postImages.js
+++ frontend/src/data/postImages.js
@@ -1,14 +1,49 @@
 /**
  * Maps imageKey from posts.json to the actual image URL.
- * Vite needs static imports for assets, so we centralize them here.
- * When you add more posts, add the import and the key below.
+ * Vite needs static imports for assets. Keys must match "imageKey" in posts.json.
  */
 import image1 from '../assets/Images Folder/Unsafe/image1.png'
+import image2 from '../assets/Images Folder/Unsafe/image2.png'
+import image3 from '../assets/Images Folder/Unsafe/image3.png'
+import image4 from '../assets/Images Folder/Unsafe/image4.png'
+import image5 from '../assets/Images Folder/Unsafe/image5.png'
+import image6 from '../assets/Images Folder/Unsafe/image6.png'
+import image7 from '../assets/Images Folder/Unsafe/image7.png'
+import image8 from '../assets/Images Folder/Unsafe/image8.png'
 import art from '../assets/Images Folder/Safe/art.png'
+import dog from '../assets/Images Folder/Safe/dog.png'
+import food from '../assets/Images Folder/Safe/food.png'
+import selfie from '../assets/Images Folder/Safe/selfie.png'
+import nature from '../assets/Images Folder/Safe/nature.png'
+import pc from '../assets/Images Folder/Safe/pc.png'
+import reading from '../assets/Images Folder/Safe/reading.png'
+import basketball from '../assets/Images Folder/Safe/basketball.png'
+import band from '../assets/Images Folder/Safe/band.png'
+import craft from '../assets/Images Folder/Safe/craft.png'
+import friends from '../assets/Images Folder/Safe/friends.png'
+import trophies from '../assets/Images Folder/Safe/trophies.png'
 
 export const postImageMap = {
   image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
   art,
+  dog,
+  food,
+  selfie,
+  nature,
+  pc,
+  reading,
+  basketball,
+  band,
+  craft,
+  friends,
+  trophies,
 }
 
 export default postImageMap
