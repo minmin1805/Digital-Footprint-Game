5e81719aa24eb6427abcd65c73d1cb5f955f9744
minmin1805 <doanlyminh2005@gmail.com>
2026-01-28 14:34:08 -0500

    started design for game page

---
+++ frontend/src/components/FriendSection.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+import person1 from '../assets/GamePage/FriendIcons/person1.png'
+import person2 from '../assets/GamePage/FriendIcons/person2.png'
+import person3 from '../assets/GamePage/FriendIcons/person3.png'
+import person4 from '../assets/GamePage/FriendIcons/person4.png'
+import person5 from '../assets/GamePage/FriendIcons/person5.png'
+import person6 from '../assets/GamePage/FriendIcons/person6.png'
+
+
+function FriendSection() {
+
+    const friendsInfo = [
+        {
+            id: 1,
+            name: "jess",
+            image: person1,
+        },
+        {
+            id: 2,
+            name: "t_om",
+            image: person2,
+        },
+        {
+            id: 3,
+            name: "kindlnd",
+            image: person3,
+        },
+        {
+            id: 4,
+            name: "mary_trg",
+            image: person4,
+        },
+        {
+            id: 5,
+            name: "vivet",
+            image: person5,
+        },
+        {
+            id: 6,
+            name: "jacksmith_",
+            image: person6,
+        }
+    ]
+  return (
+    <div className='flex items-center justify-between gap-5'>
+        {friendsInfo.map((eachFriend) => {
+            return (
+                <div key={eachFriend.id} className='flex flex-col items-center justify-center gap-2'>
+                    <img src={eachFriend.image} alt={eachFriend.name} className='w-15 h-15 rounded-full border-2 border-gray-300' />
+                    <p className='text-sm font-bold'>{eachFriend.name}</p>
+                </div>
+            )
+        })}
+    </div>
+  )
+}
+
+export default FriendSection
--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import ProgressBar from '../components/ProgressBar'
+import FriendSection from '../components/FriendSection'
 
 function GamePage() {
   return (
-    <div>
-      <h1>Game</h1>
+    <div className='flex flex-col items-center h-screen w-[70%] mx-auto bg-blue-200'>
+      
+      {/* banner and progress bar */}
+      <div className='flex flex-col items-center justify-center p-4 gap-4 bg-yellow-300 rounded-xl mt-3'>
+        <h1 className='text-3xl font-bold text-center text-blue-500'>Digital footprint detective</h1>
+        <ProgressBar />
+      </div>
+
+      <div className='w-full h-[1px] bg-gray-900 my-4'></div>
+
+      {/* friend section */}
+      <div>
+      <FriendSection />
+      </div>
     </div>
   )
 }
