acfc83652abbf08f8d977c0900ffa5476185102f
minmin1805 <doanlyminh2005@gmail.com>
2026-02-02 14:29:27 -0500

    ready for production

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,13 @@ import dotenv from "dotenv";
 import connectDB from "./lib/db.js";
 import playerRoutes from "./routes/player.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
-const app = express();
+const app = express();  
+
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: "*",
@@ -20,6 +23,14 @@ app.use("/api/players", playerRoutes);
 
 // Use 5001 to avoid conflict with macOS AirPlay Receiver on 5000
 const PORT = process.env.PORT || 8000;
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
